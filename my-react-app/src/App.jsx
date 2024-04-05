836df100251e8245469232efccd5183a02b19244
Thong <117352680+Th0ngpv@users.noreply.github.com>
2024-04-05 20:42:27 +0700

    conditional rendering in React

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,9 @@
-import Student from "./Student";
+import UserGreeting from "./UserGreeting";
 
 function App() {
   return(
     <>
-      <Student name="Sponge Bob" age="30" isStudent = {true}></Student>
-      <Student name="Who" age={21} isStudent = {false}></Student>
-      <Student name="He" age={30} isStudent = {false}></Student>
-      <Student></Student>
+      <UserGreeting isLoggedIn = {true} ></UserGreeting>
     </>
   );
 }
