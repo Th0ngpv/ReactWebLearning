08940f4c0b6b00d535302098216209cd86a5a111
Thong <117352680+Th0ngpv@users.noreply.github.com>
2024-04-05 21:06:13 +0700

    adding an array of objects to a list, popular ways to sort and filter an array

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,8 +1,25 @@
 
 function List(){
-    const fruits = ["apple","banana","orange"];
+    const fruits = [{id: 1,name:"apple",calories:95},
+                    {id: 2,name:"banana",calories:80},
+                    {id: 3,name:"orange",calories:40},
+                    {id: 4,name:"coconut",calories:110}
+    ];
 
-    const listItems = fruits.map(fruit => <li>{fruit}</li>);
+    // //some popular type of sorting an array
+    // fruits.sort((a,b) => a.name.localeCompare(b.name)); //alphabetical order
+    // fruits.sort((a,b) => b.name.localeCompare(a.name)); //reverse alphabetical order
+    // fruits.sort((a,b) => a.calories - b.calories); //ascending order
+    // fruits.sort((a,b) => b.calories - a.calories); //descending order
+    // //filter an array
+    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+    // const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
+
+    const listItems = fruits.map(fruit => 
+    <li key={fruit.id}>
+        {fruit.name}: &nbsp;
+        <b>{fruit.calories}</b>
+    </li>);
 
     return (<ul>{listItems}</ul>);
 }
