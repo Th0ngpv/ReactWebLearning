846a92d45d35aa7f707be9075f3d87c21ac71b41
Thong <117352680+Th0ngpv@users.noreply.github.com>
2024-04-05 21:54:09 +0700

    onClick event handler

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,10 @@
-import List from "./List";
+import Button from "./Button";
 
 function App() {
-  const fruits = [{id: 1,name:"apple",calories:95},
-                  {id: 2,name:"banana",calories:80},
-                  {id: 3,name:"orange",calories:40},
-                  {id: 4,name:"coconut",calories:110}
-  ];
-  const vegetables = [{id: 5,name:"potatoes",calories:95},
-                      {id: 6,name:"celery",calories:80},
-                      {id: 7,name:"carrot",calories:60},
-                      {id: 8,name:"broccoli",calories:110}
-  ];
+
   return(
     <>
-      {fruits.length > 0 ? <List items={fruits} category="fruits"></List> : null}
-      {vegetables.length > 0 ? <List items={vegetables} category="Vegetable"></List> : null}
+      <Button></Button>
     </>
 
   );
---
+++ my-react-app/src/Button.jsx
@@ -0,0 +1,20 @@
+
+function Button () {
+    let count = 0;
+    const handleClick = (name) => {
+        if (count < 3) {
+            count ++
+            console.log(`${name} you click me ${count} times`);
+        } else {
+            console.log(`${name} stop clicking me !`);
+        }
+        
+    };
+
+
+    return (
+        <button onClick={() =>handleClick("Thong")}>Click me!</button>
+    );
+}
+
+export default Button
