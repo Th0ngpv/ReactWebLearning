1e2d344a595e353beb7a39b2e28ee1dee496c3b3
Thong <117352680+Th0ngpv@users.noreply.github.com>
2024-04-06 00:03:45 +0700

    onChange event handler used in form elements

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,10 @@
-//React hook = functions that allow functional components to use 
-//             React feature without writing class components 
-//             (useState, useEffect, useContent, useCallback)
-//              usually begin the the phrase use
-
-//useState() = a React hook that create a stateful variable and 
-//             a setter function to update its value in the virtual DOM
-//             [name, setName]
-import Counter from './Counter'
+import MyComponent from './MyComponent'
 
 function App() {
   return(
-    <Counter></Counter>
+    <>
+      <MyComponent></MyComponent>
+    </>
   );
 }
 
--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,33 +1,70 @@
+//onChange = event handler used primarily with form elements
+//             ex: <input>, <textarea>, <select>, <radio>,...
+//             Triggers a function every time the value of the input changes
+
 import {useState} from 'react';
 
 const MyComponent = () => {
     const [name, setName] = useState("Guest");
-    const [age, setAge] = useState(0);
-    const [isEmployed, setIsEmployed] = useState(false);
+    const [quantity, setQuantity] = useState(0);
+    const [comment, setComment] = useState("");
+    const [payment, setPayment] = useState("");
+    const [shipping, setShipping] = useState("Delivery");
 
-    const updateName = () => {
-        setName("SpongeBob");
+    function handleNameChange(e) {
+        setName(e.target.value);
     }
-    const incrementAge = () => {
-        setAge(age + 1);
+    function handleQuantityChange(e) {
+        setQuantity(e.target.value);
+    }
+    function handleCommentChange(e) {
+        setComment(e.target.value);
+    }
+    function handlePaymentChange(e) {
+        setPayment(e.target.value);
+    }
+    function handleShippingChange(e) {
+        setShipping(e.target.value);
     }
 
-    const toggleEmployedStatus = () => {
-        setIsEmployed(!isEmployed);
-    }
-    
     return (
-    <div>
-      <p>Name: {name}</p>
-      <button onClick={updateName}>Set Name</button>
+        <div>
+            <p>Name: {name}</p>
+            <input type="text" value={name} onChange={handleNameChange} />
 
-      <p>Age: {age}</p>
-      <button onClick={incrementAge}>increase Age</button>
+            <p>Quantity: {quantity}</p>
+            <input type="number" value={quantity} onChange={handleQuantityChange} />
+            
+            <p>Comments: {comment}</p>
+            <textarea value={comment} onChange={handleCommentChange} 
+            placeholder='enter some comments'/>
 
-      <p>Age: {isEmployed ? "yes" : "no"}</p>
-      <button onClick={toggleEmployedStatus}>Toggle Status</button>
+            <p>Payment: {payment}</p>
+            <select value={payment} onChange={handlePaymentChange}>
+                <option value="">Please select an option</option>
+                <option value="Visa">Visa</option>
+                <option value="MasterCard">MasterCard</option>
+                <option value="giftCard">giftCard</option>
+            </select>
+            
+            <p>Shipping method: {shipping}</p>
+            <label>
+                <input type="radio" value="Pick Up" 
+                checked={shipping === "Pick Up"}
+                onChange={handleShippingChange} 
+               />
+                Pick Up
+            </label>
+            <label>
+                <input type="radio" value="Delivery" 
+                checked={shipping === "Delivery"}
+                onChange={handleShippingChange} 
+               />
+                Delivery
+            </label>
 
-    </div>
+
+        </div>
     );
 }
 
